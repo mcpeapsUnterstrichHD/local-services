4cf8f6315defd459a9cfd28edbf6e414926dde2a
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-02-08 04:03:53 +0100

    .

---
+++ start.sh
@@ -0,0 +1,11 @@
+#                             Online Bash Shell.
+#                 Code, Compile, Run and Debug Bash script online.
+# Write your code in this editor and press "Run" button to execute it.
+cd /Users/mahd/DEV/searxng/
+
+echo "Ich befinde mich im Verzeichnis"
+
+podman compose up -d --remove-orphans
+
+echo "Der Container läuft"
+
