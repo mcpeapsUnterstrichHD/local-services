13b1ee54e8014bad957318ec096460a6cd40bff8
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-12-13 18:01:37 +0100

    .

--- stop.sh
+++ stop.sh
@@ -1,11 +1,6 @@
 #                             Online Bash Shell.
 #                 Code, Compile, Run and Debug Bash script online.
 # Write your code in this editor and press "Run" button to execute it.
-cd /Users/mahd/DEV/searxng/
-
-echo "Ich befinde mich im Verzeichnis"
-
 docker compose down
 
 echo "Der Container läuft nicht mehr"
-
