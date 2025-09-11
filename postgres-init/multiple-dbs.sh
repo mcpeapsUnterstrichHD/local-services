2b2d573ed72317164212d4967a337fa4a67f3076
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2025-09-11 16:21:03 +0200

    .

--- postgres-init/multiple-dbs.sh
+++ postgres-init/multiple-dbs.sh
@@ -11,6 +11,12 @@ IFS=',' read -ra DBS <<< "$POSTGRES_MULTIPLE_DATABASES"
 for db in "${DBS[@]}"; do
     echo "Creating database: $db"
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-        CREATE DATABASE "$db";
+        CREATE DATABASE "$db" IF NOT EXISTS;
 EOSQL
 done
+
+
+# this will prompt for a password for the new user
+createuser --pwprompt synapse_user
+
+
